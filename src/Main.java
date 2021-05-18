4c155ec2f8aeee0e62766b784f5ff8c49324864e
Michał Gaponiuk <michal.gaponiuk@gmail.com>
2021-05-18 10:45:15 +0200

    Poprawki do rozwiazania 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-public class Main implements DoSomething {
+public class Main implements DoSomething, DoSomethingElse {
     @Override
     public void doesSomething() {
         System.out.println("I'm doing something");
+    }
+
+    @Override
+    public void doSomethingElse() {
+        System.out.println("I'm doing something else");
     }
 
     public static void main(String[] args) {
@@ -9,6 +14,6 @@ public class Main implements DoSomething {
         main.doesSomething();
 
         Main main1 = new Main();
-        main1.doesSomething();
+        main1.doSomethingElse();
     }
 }
